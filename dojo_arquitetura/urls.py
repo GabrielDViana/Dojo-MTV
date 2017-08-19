68f192f877695ce57acf2bee4d01f27d0e5cec78
Gabriel Viana <gabrielv14@gmail.com>
2017-08-19 02:24:12 -0300

    MTV working, CRUD examplified

--- dojo_arquitetura/urls.py
+++ dojo_arquitetura/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^users/', include('users.urls')),
+    url(r'^users/', include('users.urls', namespace='users')),
     url(r'^admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
-def index(request):
-    return HttpResponse("YAMERO!")
+from .models import User
+
+class UserList(ListView):
+    model = User
+
+class UserCreate(CreateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_url = reverse_lazy('users:user_list')
+
+class UserUpdate(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_url = reverse_lazy('users:user_list')
+
+class UserDelete(DeleteView):
+    model = User
+    success_url = reverse_lazy('users:user_list')
